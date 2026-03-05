72c2a051cb484d8d72db662ea493801ecde1f81c
zemberek82 <85451126+zemberek82@users.noreply.github.com>
2026-03-05 15:09:06 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:08:17.239Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T12:09:06.216Z
+Commit: Resolve timezone issues in date handling
