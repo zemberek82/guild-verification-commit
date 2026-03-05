cd6b83dff3a17db9d2f0c131af19dc50b9d4ea7d
zemberek82 <85451126+zemberek82@users.noreply.github.com>
2026-03-05 15:05:56 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:05:16.939Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T12:05:56.728Z
+Commit: Fix broken links in documentation
