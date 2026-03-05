f2e9c48ed1bbb1d90b76e444c34e6761a9708539
zemberek82 <85451126+zemberek82@users.noreply.github.com>
2026-03-05 15:09:43 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:09:17.029Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:09:43.519Z
+Commit: Fix memory leak in event handlers
