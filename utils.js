17c1d79ec258813f68cd28c8396298899281abeb
zemberek82 <85451126+zemberek82@users.noreply.github.com>
2026-03-05 15:09:53 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:09:45.789Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T12:09:53.551Z
+Commit: Fix CSS styling bug on mobile devices
