e562ace3e117c0fdb7ec3d05986036cc89404c56
zemberek82 <85451126+zemberek82@users.noreply.github.com>
2026-03-05 15:09:57 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:09:53.551Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T12:09:57.236Z
+Commit: Patch security vulnerability in authentication
