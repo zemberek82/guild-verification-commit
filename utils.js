6f0ce779ca20cfaf0312cdf9432685e1af02dac4
zemberek82 <85451126+zemberek82@users.noreply.github.com>
2026-03-05 15:08:25 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:08:19.311Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:08:25.079Z
+Commit: Fix broken links in documentation
